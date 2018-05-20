6ab76e14713bf2bdde58f5c60a181b4992cf154a
Feaskye <wangronfa@gmail.com>
2018-05-20 21:22:15 +0800

    加入api

---
+++ SkyMallCore/SkyMallCore.WebApi/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http;
+using SkyMallCore.WebApi.Models;
+
+namespace SkyMallCore.WebApi.Controllers
+{
+    /// <summary>
+    /// 首页
+    /// </summary>
+    public class HomeController : ApiController
+    {
+        /// <summary>
+        /// 获取首页内容  
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public object Index()
+        {
+            return new { data=100};
+        }
+        
+    }
+}
---
+++ SkyMallCore/SkyMallCore.WebApi/Startup.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.PlatformAbstractions;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace SkyMallCore.WebApi
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                 .SetBasePath(env.ContentRootPath)
+                 .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                 .AddEnvironmentVariables();
+
+            if (env.IsDevelopment())
+            {
+                // This will push telemetry data through Application Insights pipeline faster, allowing you to view results immediately.
+                builder.AddApplicationInsightsSettings(developerMode: true);
+            }
+            
+            Configuration = builder.Build();
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //json
+            services.AddMvc()
+                .AddJsonOptions(options=> options.SerializerSettings.ContractResolver
+                =new Newtonsoft.Json.Serialization.DefaultContractResolver());
+
+            //swagger
+            services.AddSwaggerGen(options=> {
+                options.SwaggerDoc("v1",new Info {
+                    Version ="v1",
+                    Title = "SkyMallCore.WebApi"
+                });
+
+                var basePath = PlatformServices.Default.Application.ApplicationBasePath;
+                var xmlPath = Path.Combine(basePath, "SkyMallCore.WebApi.xml");
+                options.IncludeXmlComments(xmlPath);
+            });
+
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "api/{controller=Home}/{action=Index}/{id?}");
+            });
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c=> {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "MsSystem API V1");
+            });//https://www.cnblogs.com/wms01/p/6667771.html
+
+
+        }
+    }
+}
