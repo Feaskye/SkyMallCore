4f6b2659fa1a0e23b4211e9c8a4d57fa72cb4d29
Feaskye <wangronfa@gmail.com>
2018-05-30 22:25:47 +0800

    add

---
+++ SkyMallCore/SkyMallCoreWeb/Filters/ErrorAttribute.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
+using SkyMallCore.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SkyMallCoreWeb
+{
+    public class ErrorAttribute : ExceptionFilterAttribute
+    {
+        public override void OnException(ExceptionContext context)
+        {
+            var logger = CoreProviderContext.GetLogger(context.RouteData.Values["controller"].ToString());
+            logger.LogError(context.Exception.Message);
+
+            base.OnException(context);
+
+            if (!CoreProviderContext.HostingEnvironment.IsDevelopment())
+            {
+                context.ExceptionHandled = true;
+                context.HttpContext.Response.StatusCode = 200;
+                context.Result = new ContentResult { Content = new AjaxResult { state = ResultType.error.ToString(), message = context.Exception.Message }.ToJson() };
+            }
+          
+        }
+
+
+    }
+}
---
+++ SkyMallCore/SkyMallCoreWeb/Filters/SysRoleAuthAttribute.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using SkyMallCore.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkyMallCoreWeb
+{
+    /// <summary>
+    /// 系统管理权限验证
+    /// </summary>
+    public class SysRoleAuthAttribute : ActionFilterAttribute
+    {
+        public bool Ignore { get; set; }
+        public SysRoleAuthAttribute(bool ignore = true)
+        {
+            Ignore = ignore;
+        }
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (CoreProviderContext.Provider.CurrentSysUser.IsSystem)
+            {
+                return;
+            }
+            if (Ignore == false)
+            {
+                return;
+            }
+            if (!this.ActionAuthorize(filterContext))
+            {
+                StringBuilder sbScript = new StringBuilder();
+                sbScript.Append("<script type='text/javascript'>alert('很抱歉！您的权限不足，访问被拒绝！');</script>");
+                filterContext.Result = new ContentResult() { Content = sbScript.ToString() };
+                return;
+            }
+        }
+        private bool ActionAuthorize(ActionExecutingContext filterContext)
+        {
+            var operatorProvider = CoreProviderContext.Provider.CurrentSysUser;
+            var roleId = operatorProvider.RoleId;
+            var moduleId = WebHelper.GetCookie("nfine_currentmoduleid");
+            var action = CoreProviderContext.HttpContext.Request.Headers["SCRIPT_NAME"].ToString();
+            return CoreProviderContext.GetService<SkyMallCore.Services.ISysRoleAuthorizeService>()
+                .ActionValidate(roleId, moduleId, action);
+        }
+    }
+}
